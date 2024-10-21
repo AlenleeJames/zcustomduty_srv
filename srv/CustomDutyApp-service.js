86bf244eeab352825273096513aafdcb038cc78d
Alenlee � James <james.alenleemeenvely@in.bosch.com>
2024-10-21 15:34:32 +0000

    Additon of Invoice POST API v.1

--- srv/CustomDutyApp-service.js
+++ srv/CustomDutyApp-service.js
@@ -3,8 +3,33 @@ const cds = require('@sap/cds');
 module.exports = async (srv) => 
 {        
     // Using CDS API      
-    const STIHL_B2B_K4Q100_BASIC = await cds.connect.to("STIHL_B2B_K4Q100_BASIC"); 
-      srv.on('READ', 'POVendorVH', req => STIHL_B2B_K4Q100_BASIC.run(req.query)); 
-      srv.on('READ', 'PlantVH', req => STIHL_B2B_K4Q100_BASIC.run(req.query)); 
-      srv.on('READ', 'ZA_MM_CustomDutyInvDetails', req => STIHL_B2B_K4Q100_BASIC.run(req.query)); 
+    const CustomDutyMaster = await cds.connect.to("CustomDutyInvoiceSrv"); 
+      srv.on('READ', 'POVendorVH', req => CustomDutyMaster.run(req.query)); 
+      srv.on('READ', 'PlantVH', req => CustomDutyMaster.run(req.query)); 
+      srv.on('READ', 'ZA_MM_CustomDutyInvDetails', req => CustomDutyMaster.run(req.query)); 
+
+    // Using CDS API      
+    const API_SUPPLIERINVOICE_PROCESS_SRV = await cds.connect.to("API_SUPPLIERINVOICE_PROCESS_SRV"); 
+      srv.on('READ', 'SupplierInvoice', req => API_SUPPLIERINVOICE_PROCESS_SRV.run(req.query)); 
+
+    const InvoiceSrv = await cds.connect.to("API_SUPPLIERINVOICE_PROCESS_SRV"); 
+    const { SupplierInvoice } = srv.entities;
+    //Service Call to POST Supplier Invoice
+    srv.on('PostSupplierInvoice', async(req) => {
+      // Extract the Supplier Invoice payload from the request data
+      const CustomDutyData = req.data.InvoiceData;
+      // Map the POST request on remote service
+      const InvoicePostResponse = await InvoiceSrv.run(INSERT.into(SupplierInvoice, [CustomDutyData]));      
+      // Return the response
+      return InvoicePostResponse;
+    });
+
+    srv.before ('CREATE', 'CustomDutyMaster', async (req) => {
+        
+        
+    })  
+      
+      //srv.after ('READ', 'CustomDutyMaster', each => {
+        
+      //})
 }
